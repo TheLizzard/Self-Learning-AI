abdee455d3d99d4cb81388fecafb53db61b54247
TheLizzard <64270863+TheLizzard@users.noreply.github.com>
2021-01-05 18:55:50 +0000

    Generalised the dataset class
    * Made the dataset available even if `number_of_inputs` isn't 0

--- src/training/datasets.py
+++ src/training/datasets.py
@@ -4,29 +4,43 @@ import pickle
 
 
 class TrainDataset(Sequence):
-    __slots__ = ("data", )
-
-    def __init__(self, number_vars=2):
+    def __init__(self, number_inputs=1, number_outputs=1):
         """
         `number_vars` shows how many vars there are going to be:
             2 if there is one input and one output
             3 if there is one input and two outputs
             4 if there are two inputs and two outputs
         """
-        self.number_vars = number_vars
+        self.number_inputs = number_inputs
+        self.number_outputs = number_outputs
+        self.number_vars = number_inputs+number_outputs
         self.data = []
-        for var in range(number_vars):
+        for var in range(self.number_vars):
             self.data.append([])
 
     def add(self, *vars):
         msg = "The number of variables given doesn't match what it should be."
-        assert len(vars) == len(self.data), msg
+        assert len(vars) == self.number_vars, msg
         for i, var in enumerate(vars):
             self.data[i].append(np.asarray(var, dtype="float32"))
 
+    def flush(self, slice=None):
+        if slice is None:
+            self.data = []
+            for var in range(self.number_vars):
+                self.data.append([])
+        else:
+            self.data = [_list[slice] for _list in self.data]
+
     def __getitem__(self, idx):
-        question, *answers = tuple(np.asarray(d, dtype="float32") for d in self.data)
-        return question, list(answers)
+        all = list(np.asarray(d, dtype="float32") for d in self.data)
+        questions = all[:self.number_inputs]
+        answers = all[self.number_inputs:]
+        if self.number_inputs == 1:
+            questions = questions[0]
+        if self.number_outputs == 1:
+            answers = answers[0]
+        return questions, answers
 
     def __len__(self):
         return 1
